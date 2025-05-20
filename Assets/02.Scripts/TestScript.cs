a660c5ee558ed5bc54cc07053dddd51b9c3f9d5e
lazyfoxx96 <lazyfoxx96@gmail.com>
2025-05-20 20:05:37 +0900

    인코딩 테스트
    ㄱㄱ

---
+++ Assets/02.Scripts/TestScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //인코딩 테스트!
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
