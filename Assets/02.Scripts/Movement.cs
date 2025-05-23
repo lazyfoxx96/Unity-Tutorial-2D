3c6281ac44d6769781518c7fdfcad9838208a6cc
lazyfoxx96 <lazyfoxx96@gmail.com>
2025-05-23 09:10:30 +0900

    250522수업내용
    ㅇㅇ

--- Assets/02.Scripts/Movement.cs
+++ Assets/02.Scripts/Movement.cs
@@ -16,26 +16,43 @@ public class Movement : MonoBehaviour
     //this 빼도됨
     void Update()
     {
+        /// input System (Old - Legacy)
+        /// 입력값에 대한 약속된 시스템
+        /// 이동 -> WASD, 화살표 키보드
+        /// 점프 -> Space
+        /// 총쏘기 -> 마우스 좌클릭
+
         //transform.position = transform.position + Vector3.forward * moveSpeed;
         //                      어몽어스 위치       앞쪽->(0,0,1)   속도 조절 변수
-        if(Input.GetKey(KeyCode.W)) //앞으로
-        {
-            transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
-        }
+        //if(Input.GetKey(KeyCode.W)) //앞으로
+        //{
+        //    transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
+        //}
 
-        if (Input.GetKey(KeyCode.S)) //뒤로
-        {
-            transform.position += Vector3.back * moveSpeed * Time.deltaTime;
-        }
+        //if (Input.GetKey(KeyCode.S)) //뒤로
+        //{
+        //    transform.position += Vector3.back * moveSpeed * Time.deltaTime;
+        //}
 
-        if (Input.GetKey(KeyCode.A)) // 왼쪽으로
-        {
-            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
-        }
+        //if (Input.GetKey(KeyCode.A)) // 왼쪽으로
+        //{
+        //    transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        //}
 
-        if (Input.GetKey(KeyCode.D)) // 오른쪽으로
-        {
-            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
-        }
+        //if (Input.GetKey(KeyCode.D)) // 오른쪽으로
+        //{
+        //    transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        //}
+
+        //x
+        float h = Input.GetAxis("Horizontal");
+        //z
+        float v = Input.GetAxis("Vertical");
+
+        //                        x  y  z
+        Vector3 dir = new Vector3(h, 0, v);
+        Debug.Log($"현재 입력 : {dir}");
+
+        transform.position += dir * moveSpeed * Time.deltaTime;
     }
 }
