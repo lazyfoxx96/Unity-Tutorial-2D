11621b38185c6d863c3d655331f86e4d6d458eaa
lazyfoxx96 <lazyfoxx96@gmail.com>
2025-05-26 14:39:23 +0900

    0526-1 Update

---
+++ Assets/02.Scripts/Roulette/RouletteController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour
+{
+    public float rotSpeed;
+
+    public bool isStop;
+
+    private void Start()
+    {
+        rotSpeed = 0f;        
+    }
+
+    void Update()
+    {
+        //Z축을 기준으로 회전하는 기능
+        transform.Rotate(Vector3.forward * rotSpeed);
+        //transform.Rotate(0f, 0f, rotSpeed);
+
+        //Vector3.forward = new Vector3(0f, 0f, 1f);
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            rotSpeed = 5f;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            isStop = true;
+        }
+
+        if (isStop == true)
+        {
+            //점점 속도를 줄임 2%씩
+            rotSpeed *= 0.98f; //현재 속도에서 특정 값만큼 줄이는 기능
+
+            if (rotSpeed < 0.1f)
+                rotSpeed = 0f;
+                isStop = false;
+        }
+        
+    }
+}
---
+++ Assets/02.Scripts/StudyCs/StudyPolygon.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class StudyPolygon : MonoBehaviour
+{
+    void Start()
+    {
+        Mesh mesh = new Mesh(); // 형태 데이터가 들어갈 Mesh 타입의 변수 생성
+
+        //점 4개 찍기
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3(0, 0, 0),
+            new Vector3(1, 0, 0),
+            new Vector3(0, 1, 0),
+            new Vector3(1, 1, 0)
+        };
+
+        //삼각형 순서
+        int[] triangles = new int[]
+        {
+            0, 2, 1,
+            2, 3, 1
+        };
+
+        //면의 방향
+        Vector2[] uv = new Vector2[]
+        {
+            new Vector2(0, 0),
+            new Vector2(1, 0),
+            new Vector2(0, 1),
+            new Vector2(1, 1)
+        };
+
+        //Mesh에 위에서 만든 점, 삼각형 순서, UV데이터를 적용
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.uv = uv;
+
+        //MeshFilter에 Mesh 데이터를 적용
+        MeshFilter meshFilter = this.gameObject.AddComponent<MeshFilter>();
+        meshFilter.mesh = mesh;
+
+        //MeshRenderer에 Material 데이터를 적용
+        MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
+        meshRenderer.material = new Material(Shader.Find("Sprites/Default"));
+    }
+}
